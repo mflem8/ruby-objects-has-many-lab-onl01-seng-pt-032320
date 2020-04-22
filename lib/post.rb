c834957cff10e2e533a5edfee9bbba7c6f4d738e
Matt Fleming <matthewfleming8@gmail.com>
2020-04-22 18:58:20 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,36 @@
+class Post
+  attr_accessor :author, :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    title = name
+    @@all << title
+  end
+
+  def posts
+    post = Post.new(post)
+    post.author = self
+    @posts << post
+  end
+
+  def author=(author_obj)
+    @author = author_obj
+    self.author.posts << self
+  end
+
+
+  def title(title)
+    @title = title
+    @@all << title
+  end
+
+  def Post.all
+    @@all
+  end
+
+  def author_name
+    self.author ? self.author.name : nil
+  end
+end
