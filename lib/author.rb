4a100617b9b4f73e543982f02bd4451b1acfc9f9
Matt Fleming <matthewfleming8@gmail.com>
2020-04-22 19:01:02 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,12 +19,14 @@ class Author
     @@post_count += 1
   end
 
-  def add_post_by_title(post_title)
+  def add_post_by_title=(post_title)
     post = Post.new(post_title)
     @posts << post_title
     post.author = self
     @@post_count += 1
   end
+
+  # expect(betty.posts.last.title).to eq("My Great Blog Post")
 
 
   def self.post_count
