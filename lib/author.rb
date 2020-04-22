1820f9d5bb1f8d95667f50ad1637ca9fa9d16dc7
Matt Fleming <matthewfleming8@gmail.com>
2020-04-22 19:07:05 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,8 +19,8 @@ class Author
     @@post_count += 1
   end
 
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
+  def add_post_by_title=(post_title)
+    post_title = Post.new(post_title)
     @posts << post_title
     post.author = self
     @@post_count += 1
